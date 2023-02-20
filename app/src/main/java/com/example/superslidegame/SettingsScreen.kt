112f4e97267fd03a3cee85da9c58831ba8d95254
peremunoz <73690199+peremunoz@users.noreply.github.com>
2023-02-20 19:03:46 +0100

    Added functionality to the Switch Component and changed activity type.

--- app/src/main/java/com/example/superslidegame/SettingsScreen.kt
+++ app/src/main/java/com/example/superslidegame/SettingsScreen.kt
@@ -1,20 +1,27 @@
 package com.example.superslidegame
 
-import android.content.Context
 import android.content.res.Resources
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
-import android.util.AttributeSet
-import android.view.View
-import androidx.core.content.ContextCompat
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
-class SettingsScreen(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class SettingsScreen: AppCompatActivity() {
     private val screenWidth = Resources.getSystem().displayMetrics.widthPixels
     private val screenHeight = Resources.getSystem().displayMetrics.heightPixels
 
-    override fun onDraw(canvas: Canvas) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_screen)
 
+        val light_mode_switch = findViewById<SwitchCompat>(R.id.light_mode_switch)
+
+        light_mode_switch.setOnClickListener {
+            if (light_mode_switch.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
     }
 }
