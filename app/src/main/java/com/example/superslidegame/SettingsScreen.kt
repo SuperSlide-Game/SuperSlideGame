f8e960ae24fe8820d01bde0bb572a1126bac7fc3
peremunoz <73690199+peremunoz@users.noreply.github.com>
2023-02-20 19:36:33 +0100

    Added the default switch position to the system's night mode.

--- app/src/main/java/com/example/superslidegame/SettingsScreen.kt
+++ app/src/main/java/com/example/superslidegame/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.superslidegame
 
+import android.content.res.Configuration
 import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,12 @@ class SettingsScreen: AppCompatActivity() {
         setContentView(R.layout.settings_screen)
 
         val light_mode_switch = findViewById<SwitchCompat>(R.id.light_mode_switch)
+
+        when (applicationContext.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> light_mode_switch.isChecked = false
+            Configuration.UI_MODE_NIGHT_NO -> light_mode_switch.isChecked = true
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> light_mode_switch.isChecked = true
+        }
 
         light_mode_switch.setOnClickListener {
             if (light_mode_switch.isChecked) {
