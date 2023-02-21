2b9d4efaf0e225b3f926541e685c91791258bb39
Pere <peremunozfiguerol33@gmail.com>
2023-02-21 18:55:28 +0100

    Migrated to binding for accessing resources.

--- app/src/main/java/com/example/superslidegame/GameScreen.kt
+++ app/src/main/java/com/example/superslidegame/GameScreen.kt
@@ -1,21 +1,14 @@
 package com.example.superslidegame
 
-import android.content.Context
-import android.content.res.Resources
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import com.example.superslidegame.databinding.GameScreenBinding
 
 class GameScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.game_screen)
+        val binding = GameScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Run GameDraw
         GameDraw(this, null)
--- app/src/main/java/com/example/superslidegame/SettingsScreen.kt
+++ app/src/main/java/com/example/superslidegame/SettingsScreen.kt
@@ -1,30 +1,25 @@
 package com.example.superslidegame
 
 import android.content.res.Configuration
-import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.widget.SwitchCompat
+import com.example.superslidegame.databinding.SettingsScreenBinding
 
 class SettingsScreen: AppCompatActivity() {
-    private val screenWidth = Resources.getSystem().displayMetrics.widthPixels
-    private val screenHeight = Resources.getSystem().displayMetrics.heightPixels
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_screen)
-
-        val light_mode_switch = findViewById<SwitchCompat>(R.id.light_mode_switch)
+        val binding = SettingsScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         when (applicationContext.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_YES -> light_mode_switch.isChecked = false
-            Configuration.UI_MODE_NIGHT_NO -> light_mode_switch.isChecked = true
-            Configuration.UI_MODE_NIGHT_UNDEFINED -> light_mode_switch.isChecked = true
+            Configuration.UI_MODE_NIGHT_YES -> binding.lightModeSwitch.isChecked = false
+            Configuration.UI_MODE_NIGHT_NO -> binding.lightModeSwitch.isChecked = true
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> binding.lightModeSwitch.isChecked = true
         }
 
-        light_mode_switch.setOnClickListener {
-            if (light_mode_switch.isChecked) {
+        binding.lightModeSwitch.setOnClickListener {
+            if (binding.lightModeSwitch.isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
