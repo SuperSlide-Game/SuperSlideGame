5e5231e887ad335fe431dcdff4babb9bbdd46259
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-04-20 21:29:01 +0200

    improved groupCells Class
    more getters

--- app/src/main/java/com/example/superslidegame/game/elements/CellGroup.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/CellGroup.kt
@@ -4,4 +4,10 @@ class CellGroup(val position: Int, val cells: List<GamePiece>) {
     fun getSize() : Int {
         return cells.size
     }
+    fun getPosition() : Int {
+        return position
+    }
+    fun getCell(pos:Int) : GamePiece {
+        return cells[pos]
+    }
 }
