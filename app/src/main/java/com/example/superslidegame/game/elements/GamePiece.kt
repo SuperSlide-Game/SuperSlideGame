1afe422d741cc2a8afaebab21428b025d99c48dc
Pere <peremunozfiguerol33@gmail.com>
2023-04-20 21:46:36 +0200

    Add orientation and relative order for BLUE pieces

--- app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
@@ -1,4 +1,14 @@
 package com.example.superslidegame.game.elements
 
-class GamePiece (val imgSource: Int, val size: PiecesSize) {
+class GamePiece (val type: PieceType, val orientation: Orientation?, val relativeOrder: Int) {
+    val imgSrc = type.imgSrc
+    val size = type.size
+
+    constructor(type: PieceType) : this(type, null, 0)
+
+    init {
+        if (type == PieceType.BLUE && orientation == null) {
+            throw IllegalArgumentException("Cannot create a piece of type $type without an orientation")
+        }
+    }
 }
