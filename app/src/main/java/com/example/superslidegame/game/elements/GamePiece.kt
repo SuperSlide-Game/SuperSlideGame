fe47d0a307e464e32deb68c72f50fce96555566d
Pere <peremunozfiguerol33@gmail.com>
2023-04-21 21:10:15 +0200

    Remove unused parameters

--- app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
@@ -1,9 +1,9 @@
 package com.example.superslidegame.game.elements
 
-class GamePiece (val type: PieceType, val orientation: Orientation?, val groupId: Int, val relativeOrder: Int) {
+class GamePiece(val type: PieceType, val orientation: Orientation?, val groupId: Int) {
     val imgSrc = type.imgSrc
-    val size = type.size
-    constructor(type: PieceType) : this(type, null, 0, 0)
+
+    constructor(type: PieceType) : this(type, null, 0)
 
     init {
         if (type == PieceType.BLUE && orientation == null) {
