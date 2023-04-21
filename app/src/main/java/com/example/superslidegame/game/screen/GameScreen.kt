70a1b58c878b257ff2045d41983d86cc2e43bbf4
Pere <peremunozfiguerol33@gmail.com>
2023-04-21 21:03:11 +0200

    KDoc

--- app/src/main/java/com/example/superslidegame/game/screen/GameScreen.kt
+++ app/src/main/java/com/example/superslidegame/game/screen/GameScreen.kt
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.superslidegame.databinding.GameScreenBinding
 import com.example.superslidegame.game.elements.ImageAdapter
 
+/**
+ * GameScreen is the main screen of the game.
+ * It contains the grid of tiles and the buttons to control the game.
+ */
+
 class GameScreen : AppCompatActivity() {
+    /**
+     * onCreate is called when the activity is starting.
+     * It inflates the layout and sets the adapter for the grid of tiles.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = GameScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Run GameDraw
-        //GameDraw(this, null)
         binding.gridTiles.adapter = ImageAdapter(applicationContext)
     }
-
 }
