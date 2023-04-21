99f1bd1905256cb987f8e2b24272104fe1fe4e0e
Pere <peremunozfiguerol33@gmail.com>
2023-04-21 20:54:46 +0200

    KDoc

--- app/src/main/java/com/example/superslidegame/MainActivity.kt
+++ app/src/main/java/com/example/superslidegame/MainActivity.kt
@@ -6,8 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.superslidegame.databinding.ActivityMainBinding
 import com.example.superslidegame.game.screen.GameScreen
 
-
+/**
+ * Main activity for the game.
+ * This activity is the first activity that is launched when the game is started.
+ * It contains three buttons, one to start the game, one to go to the help screen, and one to go to the settings screen.
+ * Every screen is started when the corresponding button is clicked.
+ * @constructor Creates a new main activity.
+ * @see AppCompatActivity
+ * @see GameScreen
+ * @see HelpScreen
+ * @see SettingsScreen
+ */
 class MainActivity : AppCompatActivity() {
+    /**
+     * Creates the main activity.
+     * This function is called when the activity is first created.
+     * It sets the content view to the main activity layout.
+     * It also sets the on click listeners for the buttons.
+     * @param savedInstanceState The saved instance state.
+     */
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
