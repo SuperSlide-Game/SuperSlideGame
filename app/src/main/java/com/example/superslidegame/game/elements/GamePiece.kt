82d874ab60e39a8909db59ed8d1c5bde46100194
Pere <peremunozfiguerol33@gmail.com>
2023-04-21 22:10:24 +0200

    Extracted orientation from GamePiece to PieceGroup

--- app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
@@ -1,13 +1,7 @@
 package com.example.superslidegame.game.elements
 
-class GamePiece(val type: PieceType, val orientation: Orientation?, val groupId: Int) {
+class GamePiece(val type: PieceType, val groupId: Int) {
     val imgSrc = type.imgSrc
 
-    constructor(type: PieceType) : this(type, null, 0)
-
-    init {
-        if (type == PieceType.BLUE && orientation == null) {
-            throw IllegalArgumentException("Cannot create a piece of type $type without an orientation")
-        }
-    }
+    constructor(type: PieceType) : this(type, 0)
 }
