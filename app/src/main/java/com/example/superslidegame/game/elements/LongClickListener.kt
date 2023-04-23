608ab226b41e483cc796466c4add29f6b427066c
Pere <peremunozfiguerol33@gmail.com>
2023-04-23 19:11:36 +0200

    Add 2-cell movement functions

--- app/src/main/java/com/example/superslidegame/game/elements/LongClickListener.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/LongClickListener.kt
@@ -2,12 +2,39 @@ package com.example.superslidegame.game.elements
 
 import android.content.Context
 import android.view.View
+import android.widget.Toast
+import com.example.superslidegame.game.GameLogic
 
 class LongClickListener(private val context: Context, private val positionClicked: Int, private val adapter: ImageAdapter) :
     View.OnLongClickListener {
 
+    private val gameLogic : GameLogic = GameLogic(context, adapter)
+
     override fun onLongClick(view: View?): Boolean {
-        TODO()
+
+        val pieceClicked = adapter.getPiecesState()[positionClicked]
+        val actualState = adapter.getPiecesState()
+
+        when (pieceClicked.type) {
+            PieceType.EMPTY -> {
+                Toast.makeText(context, "You cannot perform any action in an empty position", Toast.LENGTH_SHORT).show()
+            }
+            PieceType.RED -> {
+                Toast.makeText(context, "You cannot move a red piece 2 cells", Toast.LENGTH_SHORT).show()
+            } else -> {
+                val whereToMove = gameLogic.whereToMove2Cells(positionClicked, actualState)
+                if (whereToMove != null) {
+                    Toast.makeText(context,
+                        "You can move this piece to position: $whereToMove", Toast.LENGTH_SHORT).show()
+                    gameLogic.move2Cells(positionClicked, whereToMove, actualState)
+                    adapter.updateBoard()
+                    //
+                } else {
+                    Toast.makeText(context, "You cannot move this piece", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        return true
     }
 
 }
