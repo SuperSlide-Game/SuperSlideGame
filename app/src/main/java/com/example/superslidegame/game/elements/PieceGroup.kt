5564bc095da7187a4442213a11775ebd8d786ffc
Pere <peremunozfiguerol33@gmail.com>
2023-04-23 19:53:18 +0200

    Important restriction documented

--- app/src/main/java/com/example/superslidegame/game/elements/PieceGroup.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/PieceGroup.kt
@@ -5,6 +5,9 @@ package com.example.superslidegame.game.elements
  * @param id the id of the group
  * @param pieces the pieces that form the group
  */
+
+// It is very important that when defining game levels, if you create an HORIZONTAL group, the first piece of the group is the leftmost one,
+// and if you create a VERTICAL group, the first piece of the group is the topmost one.
 data class PieceGroup(val id: Int, val orientation: Orientation?, val pieces: MutableList<GamePiece>) {
     constructor(id: Int, pieces: MutableList<GamePiece>) : this(id, null, pieces)
 }
