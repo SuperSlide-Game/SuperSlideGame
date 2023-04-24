c8f6a4d58ba9733ccf4557c3c11518050636e782
Pere <peremunozfiguerol33@gmail.com>
2023-04-24 20:12:12 +0200

    Create adapter for the levels ListView

---
+++ app/src/main/java/com/example/superslidegame/game/elements/LevelListAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.superslidegame.game.elements
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.widget.BaseAdapter
+import android.widget.Button
+import com.example.superslidegame.game.levels.GameLevel
+import com.example.superslidegame.game.screen.SelectLevel
+
+class LevelListAdapter(private val activity: Activity) : BaseAdapter() {
+
+    private val maxLevel = GameLevel.MAX_LEVEL
+
+    override fun getCount(): Int {
+        return maxLevel
+    }
+
+    override fun getItem(p0: Int): Any? {
+        return null
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val button : Button
+        if (convertView == null) {
+            button = Button(activity.baseContext)
+            button.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+            button.text = String.format("Level %d", position + 1)
+            button.setOnClickListener(LevelSelectorClickListener(position, activity as SelectLevel))
+        } else {
+            button = convertView as Button
+        }
+        return button
+    }
+}
