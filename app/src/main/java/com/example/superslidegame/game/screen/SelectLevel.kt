f410215f5a228a5d282db01089ceae9ef33ba2a1
Pere <peremunozfiguerol33@gmail.com>
2023-04-24 20:13:38 +0200

    Change some calls in OnCreate method for adapting to the ListView change

--- app/src/main/java/com/example/superslidegame/game/screen/SelectLevel.kt
+++ app/src/main/java/com/example/superslidegame/game/screen/SelectLevel.kt
@@ -3,13 +3,8 @@ package com.example.superslidegame.game.screen
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.example.superslidegame.databinding.ActivityMainBinding
-import com.example.superslidegame.databinding.GameScreenBinding
 import com.example.superslidegame.databinding.SelLevelBinding
-import com.example.superslidegame.game.animations.AnimationHelper
-import com.example.superslidegame.game.elements.ImageAdapter
-import com.example.superslidegame.game.levels.GameLevel
+import com.example.superslidegame.game.elements.LevelListAdapter
 
 /**
  * GameScreen is the level selector screen of the game
@@ -17,18 +12,27 @@ import com.example.superslidegame.game.levels.GameLevel
 
 class SelectLevel : AppCompatActivity() {
 
+    var selectedLevel : Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = SelLevelBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.levelList.adapter = LevelListAdapter(this)
+
         val intent = Intent(this, GameScreen::class.java)
-        binding.button3.setOnClickListener {
-            intent.putExtra("sel_level", "1")
-            startActivity(intent)
-        }
-        binding.button4.setOnClickListener {
-            intent.putExtra("sel_level", "2")
-            startActivity(intent)
+
+        binding.playButtonLevelSelector.setOnClickListener {
+            if (selectedLevel != null) {
+                intent.putExtra("level", selectedLevel)
+                startActivity(intent)
+            }
         }
     }
+
+    fun setLevel(level: Int) {
+        selectedLevel = level
+    }
+
 }
