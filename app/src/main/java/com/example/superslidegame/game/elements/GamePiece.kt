8300078ffc35f28e5017cb01908d0144c3cfc22e
Pere <peremunozfiguerol33@gmail.com>
2023-04-26 18:09:51 +0200

    GamePiece implements Parcelable

--- app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
@@ -1,7 +1,9 @@
 package com.example.superslidegame.game.elements
 
-class GamePiece(val type: PieceType, val groupId: Int) {
-    var imgSrc = type.imgSrc
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
+class GamePiece(val type: PieceType, val groupId: Int, val imgSrc: Int = type.imgSrc) : Parcelable {
     constructor(type: PieceType) : this(type, 0)
 }
