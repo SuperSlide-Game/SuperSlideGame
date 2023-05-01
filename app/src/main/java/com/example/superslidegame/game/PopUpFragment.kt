3a3ee71c2f68785c3828f104031081f8ed14b3b4
Pere <peremunozfiguerol33@gmail.com>
2023-05-01 19:46:55 +0200

    Set click listeners for popup buttons

--- app/src/main/java/com/example/superslidegame/game/PopUpFragment.kt
+++ app/src/main/java/com/example/superslidegame/game/PopUpFragment.kt
@@ -1,27 +1,42 @@
 package com.example.superslidegame.game
 
-import android.app.AlertDialog
-import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import com.example.superslidegame.R
+import com.example.superslidegame.log.screen.LogScreen
 
 class PopUpFragment : DialogFragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pop_up, container, false)
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
+        val view = inflater.inflate(R.layout.fragment_pop_up, container, false)
+        val menuButton : Button = view.findViewById(R.id.menu_button_win)
+        val nextLevelButton : Button = view.findViewById(R.id.next_level_button_win)
+        val logButton : Button = view.findViewById(R.id.log_button_win)
 
+        menuButton.setOnClickListener {
+            activity?.finish()
+        }
+
+        nextLevelButton.setOnClickListener {
+            //
+        }
+
+        logButton.setOnClickListener {
+            val intent = Intent(activity, LogScreen::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+
+        return view
+    }
 }
--- app/src/main/java/com/example/superslidegame/game/screen/TimeUpFragment.kt
+++ app/src/main/java/com/example/superslidegame/game/screen/TimeUpFragment.kt
@@ -1,11 +1,14 @@
 package com.example.superslidegame.game.screen
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import com.example.superslidegame.R
+import com.example.superslidegame.log.screen.LogScreen
 
 
 class TimeUpFragment : DialogFragment() {
@@ -15,8 +18,22 @@ class TimeUpFragment : DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(com.example.superslidegame.R.layout.fragment_time_up, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_time_up, container, false)
+        val menuButton : Button = view.findViewById(R.id.menu_button_lost)
+        val logButton : Button = view.findViewById(R.id.log_button_lost)
+
+        menuButton.setOnClickListener {
+            activity?.finish()
+        }
+
+        logButton.setOnClickListener {
+            val intent = Intent(activity, LogScreen::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+
+        return view
     }
 
 }
