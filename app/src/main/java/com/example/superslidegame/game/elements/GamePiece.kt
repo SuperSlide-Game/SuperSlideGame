e79699c08f063a331c816096f5afef6bfd974344
Pere <peremunozfiguerol33@gmail.com>
2023-05-02 21:03:36 +0200

    Change attribute order for improving game piece creation

--- app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
+++ app/src/main/java/com/example/superslidegame/game/elements/GamePiece.kt
@@ -4,6 +4,6 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-class GamePiece(val type: PieceType, val groupId: Int, val imgSrc: Int = type.imgSrc, val rotation: Float = 0f) : Parcelable {
+class GamePiece(val type: PieceType, val groupId: Int, val rotation: Float = 0f, val imgSrc: Int = type.imgSrc) : Parcelable {
     constructor(type: PieceType) : this(type, 0)
 }
