85d2c15d393119c72392db356af17cddb4f8bec5
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-05-24 19:10:08 +0200

    created gameDao

---
+++ app/src/main/java/com/example/superslidegame/game/GameDao.kt
@@ -0,0 +1,18 @@
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.superslidegame.game.entities.Game
+
+@Dao
+interface GameDao {
+
+    @Query("SELECT * FROM game_table ORDER BY level ASC")
+    fun getAlphabetizedWords(): List<Game>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Game)
+
+    @Query("DELETE FROM game_table")
+    suspend fun deleteAll()
+}
--- app/src/main/java/com/example/superslidegame/game/entities/Game.kt
+++ app/src/main/java/com/example/superslidegame/game/entities/Game.kt
@@ -1,5 +1,8 @@
 package com.example.superslidegame.game.entities
 
+import androidx.room.Entity
+
+@Entity(tableName = "game_table")
 data class Game(val level: Int, val score: Int, val time: Int) {
 
 }
