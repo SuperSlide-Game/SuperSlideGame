79cd10e7ee8c8f62e53a60def5583e7a1008cc70
Pere <peremunozfiguerol33@gmail.com>
2023-05-29 19:03:39 +0200

    Add more attributes to save in db

--- app/src/main/java/com/example/superslidegame/game/entities/Game.kt
+++ app/src/main/java/com/example/superslidegame/game/entities/Game.kt
@@ -1,8 +1,13 @@
 package com.example.superslidegame.game.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "game_table")
-data class Game(val level: Int, val score: Int, val time: Int) {
-
-}
+data class Game(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val nickname: String,
+    val level: Int,
+    val won: Boolean,
+    val time: Int,
+    val moves: Int)
