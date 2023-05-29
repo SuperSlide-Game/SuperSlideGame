d888f45842ce6bf9d0c78242ed27c5169586341d
Pere <peremunozfiguerol33@gmail.com>
2023-05-29 19:18:27 +0200

    Create new HistoryActivity with its layout

---
+++ app/src/main/java/com/example/superslidegame/history/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.superslidegame.history
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.superslidegame.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
