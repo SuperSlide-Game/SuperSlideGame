a5914ede81699231f5aef76d32159ebb34266a21
Pere <peremunozfiguerol33@gmail.com>
2023-05-30 19:51:37 +0200

    Created function for updating the fragment or starting the new activity when clicking a game record

--- app/src/main/java/com/example/superslidegame/history/AccessDBActivity.kt
+++ app/src/main/java/com/example/superslidegame/history/AccessDBActivity.kt
@@ -1,8 +1,12 @@
 package com.example.superslidegame.history
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.superslidegame.R
 import com.example.superslidegame.databinding.ActivityAccessDbBinding
+import com.example.superslidegame.fragments.RegFrag
+import com.example.superslidegame.game.entities.Game
 
 class AccessDBActivity : AppCompatActivity() {
 
@@ -11,5 +15,24 @@ class AccessDBActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    fun launchDetailsActivity(game: Game?) {
+        val regFragment = supportFragmentManager.findFragmentById(R.id.register_fragment) as RegFrag?
+        if (regFragment != null) {
+            regFragment.update(game)
+        } else {
+            val intent = Intent(this, DetailRegActivity::class.java)
+            intent.putExtra("game", game)
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        var INSTANCE: AccessDBActivity? = null
+    }
+
+    init {
+        INSTANCE = this
     }
 }
