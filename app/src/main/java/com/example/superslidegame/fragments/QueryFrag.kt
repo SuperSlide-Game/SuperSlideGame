9522811f64cde27ca8e72d33dc14b6e2d295ac40
Pere <peremunozfiguerol33@gmail.com>
2023-05-30 21:31:01 +0200

    Add information from db to adapter

--- app/src/main/java/com/example/superslidegame/fragments/QueryFrag.kt
+++ app/src/main/java/com/example/superslidegame/fragments/QueryFrag.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.superslidegame.database.GameListAdapter
+import com.example.superslidegame.database.GameViewModel
+import com.example.superslidegame.database.GameViewModelFactory
+import com.example.superslidegame.database.GamesApplication
 import com.example.superslidegame.databinding.QueryFragmentBinding
 
 class QueryFrag : Fragment() {
 
     private val binding by lazy { QueryFragmentBinding.inflate(layoutInflater) }
+
+    private val gameViewModel: GameViewModel by viewModels {
+        GameViewModelFactory((activity?.application as GamesApplication).repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,5 +34,10 @@ class QueryFrag : Fragment() {
         val adapter = GameListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        gameViewModel.allGames.observe(viewLifecycleOwner) { games ->
+            // Update the cached copy of the games in the adapter.
+            games.let { adapter.submitList(it) }
+        }
     }
 }
