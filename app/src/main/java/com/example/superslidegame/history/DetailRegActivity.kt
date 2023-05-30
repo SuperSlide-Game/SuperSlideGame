0a5a7867eb6ddc190ebd4a75ca406538a5ec0aec
Pere <peremunozfiguerol33@gmail.com>
2023-05-30 19:50:42 +0200

    Update the fragment when activity is created

--- app/src/main/java/com/example/superslidegame/history/DetailRegActivity.kt
+++ app/src/main/java/com/example/superslidegame/history/DetailRegActivity.kt
@@ -2,7 +2,10 @@ package com.example.superslidegame.history
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.superslidegame.R
 import com.example.superslidegame.databinding.ActivityDetailRegBinding
+import com.example.superslidegame.fragments.RegFrag
+import com.example.superslidegame.game.entities.Game
 
 class DetailRegActivity : AppCompatActivity() {
 
@@ -11,5 +14,8 @@ class DetailRegActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val game = intent.getParcelableExtra("game") as Game?
+        val regFragment = supportFragmentManager.findFragmentById(R.id.register_fragment) as RegFrag
+        regFragment.update(game)
     }
 }
