14813c9c2cbae220837284c6d79f494aaeb25e56
Pere <peremunozfiguerol33@gmail.com>
2023-05-30 19:36:56 +0200

    Move fragment to fragments folder

--- app/src/main/java/com/example/superslidegame/fragments/QueryFrag.kt
+++ app/src/main/java/com/example/superslidegame/fragments/QueryFrag.kt
@@ -1,4 +1,4 @@
-package com.example.superslidegame.database
+package com.example.superslidegame.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.superslidegame.database.GameListAdapter
 import com.example.superslidegame.databinding.QueryFragmentBinding
 
 class QueryFrag : Fragment() {
